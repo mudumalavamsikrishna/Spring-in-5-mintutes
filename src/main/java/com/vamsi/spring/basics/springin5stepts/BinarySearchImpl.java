367dac106470d23ebf8462659d077408e95252ea
mudumalavamsikrishna <mudumalavamsikrishna@gmail.com>
2018-03-30 10:44:10 +0530

    few things are updated

--- src/main/java/com/vamsi/spring/basics/springin5stepts/BinarySearchImpl.java
+++ src/main/java/com/vamsi/spring/basics/springin5stepts/BinarySearchImpl.java
@@ -1,12 +1,25 @@
 package com.vamsi.spring.basics.springin5stepts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearchImpl {
 
-	public int binarySearch(int[] numbers, int numberToSearchFor) {
-		
+	@Autowired
+	private SortAlgorithm sortAlgorithm;
 
-		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
-		int[] sortNumbers = bubbleSortAlgorithm.sort(numbers);
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+
+		int[] sortNumbers = sortAlgorithm.sort(numbers);
+
+		System.out.println(sortAlgorithm);
+
 		// Searchh Array
 
 		return 3;
